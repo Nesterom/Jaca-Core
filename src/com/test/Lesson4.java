5b7a2fd97e6899cd50face99c7cdbf0c606ef9af
romannesterenko <nesterrn@gmail.com>
2021-09-04 10:54:19 +0300

    intermidiate

--- src/com/test/Lesson4.java
+++ src/com/test/Lesson4.java
@@ -4,6 +4,7 @@ public class Lesson4 {
     public static void main (String[] args) {
         for (int number = 1; number <= 10; number++) {
             System.out.println ("number = " + number);
+
         }
     }
 }
