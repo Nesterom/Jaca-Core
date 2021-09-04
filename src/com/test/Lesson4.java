385fc272633c0075f6a09c5c95f88e09d5250fd8
Nesterom <nesterrn@gmail.com>
2021-09-04 19:01:41 +0300

    intermidiate

--- src/com/test/Lesson4.java
+++ src/com/test/Lesson4.java
@@ -1,10 +1,15 @@
 package com.test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Lesson4 {
     public static void main (String[] args) {
-        for (int number = 1; number <= 10; number++) {
-            System.out.println ("number = " + number);
+       int[] array = {1, 2, 3, 5, 6, 7};
 
+        System.out.println ();
+        for (int i:array) {
+            System.out.println (Arrays.toString (array));
         }
     }
 }
