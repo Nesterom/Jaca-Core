f5781800d166f8aaf042cfba1fbd40190b77c7c4
Nesterom <nesterrn@gmail.com>
2021-09-04 20:03:07 +0300

    new

--- src/com/test/Lesson4.java
+++ src/com/test/Lesson4.java
@@ -10,7 +10,7 @@ public class Lesson4 {
         System.out.println ();
         for (int i:array) {
             System.out.println (Arrays.toString (array));
-
+            System.out.println ("New test");
             System.out.println ("Push");
         }
     }
