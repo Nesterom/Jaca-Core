899c8f352ac7505d1331eec51eb49e0749c00582
Nesterom <nesterrn@gmail.com>
2021-09-05 08:28:51 +0300

    HT1 ready

---
+++ src/com/test/HT1.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class HT1 {
+    public static void main (String[] args) {
+        int[] array = {1, 2, 3, 5, 6};
+        int x = 6;
+        int res = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != x) {
+                res = res + array[i];
+            }
+        }
+        System.out.println (res);
+    }
+}
