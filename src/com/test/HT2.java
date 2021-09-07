956297c866f491dc60cd702c1450042468a364d5
Nesterom <nesterrn@gmail.com>
2021-09-07 14:42:36 +0300

    Auto fill of array by Fibonacci numbers

--- src/com/test/HT2.java
+++ src/com/test/HT2.java
@@ -2,13 +2,19 @@ package com.test;
 
 public class HT2 {
     public static void main (String[] args) {
-        int[] array = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946};
         int index = 20;
+        int[] array = new int[index];
         int res = 0;
+        array[0] = 0;
+        array[1] = 1;
+
+        for (int i = 2; i < index; i++) {
+            array[i] = array[i - 1] + array [i - 2];
+        }
 
         for (int i = 0; i < index; i = i + 3) {
             res = res + array[i];
         }
-        System.out.println (res);
+        System.out.println(res);
     }
 }
