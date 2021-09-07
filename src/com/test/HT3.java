372c6372b1f2d597dd65387e2e71c7ada79b0752
Nesterom <nesterrn@gmail.com>
2021-09-07 14:46:26 +0300

    variable j was removed

--- src/com/test/HT3.java
+++ src/com/test/HT3.java
@@ -7,9 +7,8 @@ public class HT3 {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         int revers [] = new int[array.length];
 
-        for (int i = 0, j = array.length - 1; i < array.length; i++){
-            revers[j] = array[i];
-            j--;
+        for (int i = 0; i < array.length; i++){
+            revers[array.length - 1 - i] = array[i];
         }
 
         System.out.println (Arrays.toString (revers));
